b30d8222aa2163963c80da115fe691fbc7d7aaa6
phbui <101759075+phbui@users.noreply.github.com>
2023-03-06 14:01:26 -0500

    added writings and text hover effect

--- js/fade-out.js
+++ js/fade-out.js
@@ -1,25 +1,54 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var links = document.querySelectorAll('.header-right li a');
-    var header = document.querySelector('header');
-    var body = document.querySelector('body');
-  
-    // Fade in the header and body elements
-    setTimeout(function() {
-      header.style.backgroundColor = '#000000';
-      body.style.transition = 'opacity 1s ease-out';
-      body.style.opacity = '1';
-    }, 500); // delay for 0.5 seconds (500 milliseconds)
-  
-    // Add click event listener to each link
-    for (var i = 0; i < links.length; i++) {
-      links[i].addEventListener('click', function(event) {
+  var links = document.querySelectorAll('.header-right li a');
+  var logoLink = document.querySelector('.header-left a');
+  var leftLinks = document.querySelectorAll('.left a');
+  var header = document.querySelector('.alt-header');
+  var body = document.querySelector('body');
+
+  body.style.opacity = '0';
+
+  // Fade in the header and body elements
+  setTimeout(function() {
+    header.classList.add('black');
+    body.style.opacity = '1';
+    body.style.backgroundColor = 'white';
+  }, 100);
+
+  // Add click event listener to each link
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function(event) {
+      if (event.target.href.indexOf('#') === -1){
         event.preventDefault(); // prevent the default link behavior
-        header.style.backgroundColor = '#FFFFFF'; // set the header background color to white
+        header.classList.remove('black'); // add the "black" class to the header element
         body.style.opacity = '0';
         setTimeout(function() {
           window.location.href = event.target.href; // navigate to the link's href after a delay
-        }, 1000); // delay for 1 second (1000 milliseconds)
-      });
-    }
-  });
-  
+        }, 2000);
+      }
+    });
+  }
+
+    // Add click event listener to logo link
+    logoLink.addEventListener('click', function(event) {
+      event.preventDefault(); // prevent the default link behavior
+      header.classList.add('black'); // add the "black" class to the header element
+      body.style.opacity = '0';
+      setTimeout(function() {
+        window.location.href = logoLink.href; // navigate to the link's href after a delay
+      }, 2000);
+    });
+
+    // Add click event listener to each link in the left column
+  for (var j = 0; j < leftLinks.length; j++) {
+    leftLinks[j].addEventListener('click', function(event) {
+      if (event.target.href.indexOf('#') === -1){
+        event.preventDefault(); // prevent the default link behavior
+        header.classList.remove('black'); // add the "black" class to the header element
+        body.style.opacity = '0';
+        setTimeout(function() {
+          window.location.href = event.target.href; // navigate to the link's href after a delay
+        }, 2000);
+      }
+    });
+  }
+});
