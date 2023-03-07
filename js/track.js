3db4a156f7224d92667af408e883fb9db1babe4d
phbui <101759075+phbui@users.noreply.github.com>
2023-03-06 23:09:56 -0500

    mobile overflow-x fixed itself?

--- js/track.js
+++ js/track.js
@@ -1,75 +1,71 @@
-const isMobile = /Mobile/.test(navigator.userAgent);
+const track = document.getElementById("artwork-track");
+const artworks = document.querySelectorAll(".artwork");
 
-if (!isMobile) {
-  const track = document.getElementById("artwork-track");
-  const artworks = document.querySelectorAll(".artwork");
+const handleOnDown = (e) => {
+  if (e.touches) {
+    track.dataset.mouseDownAt = e.touches[0].clientX;
+  } else {
+    track.dataset.mouseDownAt = e.clientX;
+  }
+};
 
-  const handleOnDown = (e) => {
-    if (e.touches) {
-      track.dataset.mouseDownAt = e.touches[0].clientX;
-    } else {
-      track.dataset.mouseDownAt = e.clientX;
-    }
-  };
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.percentage;
+};
 
-  const handleOnUp = () => {
-    track.dataset.mouseDownAt = "0";
-    track.dataset.prevPercentage = track.dataset.percentage;
-  };
+const handleOnMove = (e) => {
+  if (track.dataset.mouseDownAt === "0") return;
 
-  const handleOnMove = (e) => {
-    if (track.dataset.mouseDownAt === "0") return;
+  const mouseDelta =
+    parseFloat(track.dataset.mouseDownAt) -
+    (e.touches ? e.touches[0].clientX : e.clientX);
+  const maxDelta = window.innerWidth;
 
-    const mouseDelta =
-      parseFloat(track.dataset.mouseDownAt) -
-      (e.touches ? e.touches[0].clientX : e.clientX);
-    const maxDelta = window.innerWidth;
+  const percentage = (mouseDelta / maxDelta) * -100;
+  const nextPercentageUnconstrained =
+    parseFloat(track.dataset.prevPercentage) + percentage;
+  const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 2.5), -100);
 
-    const percentage = (mouseDelta / maxDelta) * -100;
-    const nextPercentageUnconstrained =
-      parseFloat(track.dataset.prevPercentage) + percentage;
-    const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 2.5), -100);
+  track.dataset.percentage = nextPercentage;
 
-    track.dataset.percentage = nextPercentage;
+  track.animate(
+    {
+      transform: `translateX(${nextPercentage}%)`,
+    },
+    { duration: 1200, fill: "forwards" }
+  );
 
-    track.animate(
+  artworks.forEach((artwork) => {
+    artwork.animate(
       {
-        transform: `translateX(${nextPercentage}%)`,
+        objectPosition: `${100 + nextPercentage}% center`,
       },
       { duration: 1200, fill: "forwards" }
     );
-
-    artworks.forEach((artwork) => {
-      artwork.animate(
-        {
-          objectPosition: `${100 + nextPercentage}% center`,
-        },
-        { duration: 1200, fill: "forwards" }
-      );
-    });
-  };
-
-  /* Add this code to animate the nextPercentage variable on page load */
-  document.addEventListener("DOMContentLoaded", () => {
-    let nextPercentage = 100;
-    const animateNextPercentage = () => {
-      nextPercentage -= 1;
-      if (nextPercentage >= -6) {
-        track.style.transform = `translateX(${nextPercentage}%)`;
-        artworks.forEach((artwork) => {
-          artwork.style.objectPosition = `${100 + nextPercentage}% center`;
-        });
-        requestAnimationFrame(animateNextPercentage);
-      }
-    };
-    requestAnimationFrame(animateNextPercentage);
   });
+};
 
-  /* Had to add extra lines for touch events */
-  window.addEventListener("mousedown", handleOnDown);
-  window.addEventListener("touchstart", handleOnDown);
-  window.addEventListener("mouseup", handleOnUp);
-  window.addEventListener("touchend", handleOnUp);
-  window.addEventListener("mousemove", handleOnMove);
-  window.addEventListener("touchmove", handleOnMove);
-}
+/* Add this code to animate the nextPercentage variable on page load */
+document.addEventListener("DOMContentLoaded", () => {
+  let nextPercentage = 100;
+  const animateNextPercentage = () => {
+    nextPercentage -= 1;
+    if (nextPercentage >= -6) {
+      track.style.transform = `translateX(${nextPercentage}%)`;
+      artworks.forEach((artwork) => {
+        artwork.style.objectPosition = `${100 + nextPercentage}% center`;
+      });
+      requestAnimationFrame(animateNextPercentage);
+    }
+  };
+  requestAnimationFrame(animateNextPercentage);
+});
+
+/* Had to add extra lines for touch events */
+window.addEventListener("mousedown", handleOnDown);
+window.addEventListener("touchstart", handleOnDown);
+window.addEventListener("mouseup", handleOnUp);
+window.addEventListener("touchend", handleOnUp);
+window.addEventListener("mousemove", handleOnMove);
+window.addEventListener("touchmove", handleOnMove);
